4589d1ea93c905eb6d157778135270da8047a723
Andrianto Putro <andri.c.putro@gmail.com>
2024-10-17 16:28:33 +0700

    add model and migration

---
+++ database/migrations/2024_10_17_083025_create_gym_testimonials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gym_testimonials', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string("occupation")->nullable();
+            $table->text("message")->nullable();
+            $table->string("photo")->nullable();
+            $table->foreignId("gym_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gym_testimonials');
+    }
+};
---
+++ database/migrations/2024_10_17_090844_create_subscribe_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscribe_transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("booking_trx_id");
+            $table->string("name");
+            $table->string("phone");
+            $table->string("email");
+            $table->string("proof");
+            $table->unsignedBigInteger("total_amount");
+            $table->date("started_at");
+            $table->date("ended_at");
+            $table->unsignedBigInteger("duration");
+            $table->boolean("is_paid")->default(false);
+            $table->foreignId("subscribe_package_id")->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscribe_transactions');
+    }
+};
